eeb83251d6aaea5923728e29c44fc48aee029c59
Vladimir Veraks <vladimirveraksa@gmail.com>
2020-03-04 11:57:10 +0300

    feat: expression-calculator done

--- src/index.js
+++ src/index.js
@@ -1,10 +1,42 @@
-function eval() {
-    // Do not use eval!!!
-    return;
-}
-
+// function eval() {
+//     // Do not use eval!!!
+//     return;
+// }
+//
 function expressionCalculator(expr) {
-    // write your solution here
+
+    let expArr = expr.trim().replace(/["']/g,'').split(' ');
+    let expObj = {};
+    let expRes = '';
+    let expConstructor = null;
+
+    const add = (key, value, obg) => {
+        obg['a'+ key] = value;
+    };
+
+    expArr.forEach((elem, index) => {
+        if (elem !== ''){
+            add(index, elem, expObj);
+        }
+        return;
+    });
+
+    for (let keys in expObj) {
+        expRes += `${expObj[keys]}`;
+    };
+
+    if(expRes.indexOf('/0') !== -1){
+        throw new Error("TypeError: Division by zero.");
+    }
+
+    if ( expRes.match(/[^(]/g).length != expRes.match(/[^)]/g).length ) {
+        throw new Error("ExpressionError: Brackets must be paired");
+    }
+
+    expConstructor = new Function(`return ${expRes}`);
+
+    return expConstructor();
+
 }
 
 module.exports = {
